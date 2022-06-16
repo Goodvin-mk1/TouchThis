436d388409b79ed4dfa33ffe39528620f1283612
Goodvin-mk1 <avetis222@gmail.com>
2022-06-16 17:04:40 +0300

    init commit

---
+++ 3_3.py
@@ -0,0 +1,8 @@
+# Отформатированное приветственное сообщение
+#
+print("Enter your name, age and city: ")
+name = input("Name: ").capitalize()
+age = input("Age: ")
+city = input("City: ").capitalize()
+print(f"Hi everyone. My name is {name}. I'm {age} years old. And i'm live in {city} city.\n")
+print("Hi everyone. My name is {}. I'm {} years old. And i'm live in {} city.".format(name, age, city))
