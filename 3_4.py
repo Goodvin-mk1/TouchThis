e0c7ef627895a15219ea60eca410a0b6f58af98c
Goodvin-mk1 <avetis222@gmail.com>
2022-06-17 21:43:37 +0300

    some changes

--- 3_4.py
+++ 3_4.py
@@ -1,10 +1,9 @@
-# Посчитать кол-во положительных
-# и отрицательных чисел
-print("Enter three nums: ")
-num_a = input("a=")
-num_b = input("b=")
-num_c = input("c=")
-num_count = 3
-cash_string_nums = str([num_a, num_b, num_c])
-negative = cash_string_nums.count('-')
-print(f"You have {num_count - negative} positive and {negative} negative nums")
+
+a = int(int(input("a = ")) < 0)
+b = int(int(input("b = ")) < 0)
+c = int(int(input("c = ")) < 0)
+
+count = 3
+result = a + b + c
+print(f"+: {3 - result}")
+print(f"-: {result}")
