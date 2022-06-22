8be0a2428bdea8551c685db7b7711a4062b1c51c
Goodvin-mk1 <avetis222@gmail.com>
2022-06-22 12:26:55 +0300

    some changes

--- 4_3.py
+++ 4_3.py
@@ -1,3 +1,4 @@
 count_users = int(input('Enter count of new users: '))
-my_dict = {i: [input(f'{i} user name: '), input(f'{i} user email: ')] for i in range(1, count_users + 1)}
+my_dict = {i: {'user:' + input(f'{i} user name: '), 'email:' + input(f'{i} user email: ')} for i in
+           range(1, count_users + 1)}
 print(my_dict)
