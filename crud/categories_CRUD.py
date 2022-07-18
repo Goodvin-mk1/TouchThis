2bda693211245f294c57ab8669df1e8ff695db0e
Goodvin-mk1 <avetis222@gmail.com>
2022-07-18 16:33:35 +0300

    changes

--- crud/categories_CRUD.py
+++ crud/categories_CRUD.py
@@ -1,5 +1,5 @@
 import sqlite3
-from schemes import CategoryScheme
+from schemes.categories import CategoryScheme
 from schemes.categories import CategoryInDBScheme
 
 conn = sqlite3.connect("shop.db")
@@ -9,14 +9,15 @@ cur = conn.cursor()
 class CategoryCRUD:
 
     @staticmethod
-    def add(category: CategoryScheme):
+    def add(category: CategoryScheme) -> None:
         cur.execute("""
-            INSERT INTO category(name, parent_id, is_published);
+            INSERT INTO category(name, parent_id, is_published)
+            VALUES(?, ?, ?);
         """, (category.name, category.parent_id, category.is_published))
         conn.commit()
 
     @staticmethod
-    def get(category_id: int) -> CategoryInDBScheme:
+    def get(category_id: CategoryScheme) -> CategoryInDBScheme:
         cur.execute(f"""
             SELECT * FROM categories WHERE id = {category_id}
         """)
@@ -28,3 +29,29 @@ class CategoryCRUD:
             name=result[3]
         )
         return category
+
+    @staticmethod
+    def get_all() -> list[CategoryInDBScheme]:
+        cur.execute("""
+        SELECT * FROM categories
+        """)
+        result = cur.fetchall()
+        return result
+
+    @staticmethod
+    def update(category_id: int, category: CategoryScheme) -> None:
+        cur.execute(f"""
+        UPDATE categories SET
+        parent_id={category.parent_id}
+        is_published={category.is_published}
+        name={category.name}
+        WHERE id = {category_id}
+        """)
+        conn.commit()
+
+    @staticmethod
+    def delete(category_id: int) -> None:
+        cur.execute(f"""
+        DELETE FROM categories WHERE id = {category_id}
+        """)
+        conn.commit()
