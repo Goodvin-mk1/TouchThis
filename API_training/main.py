2850632867b75899813aee06bffa87a54174b0ac
Goodvin-mk1 <61112569+Goodvin-mk1@users.noreply.github.com>
2022-07-27 20:43:06 +0300

    call func

--- API_training/main.py
+++ API_training/main.py
@@ -7,3 +7,4 @@ street: str = input("Enter street: ")
 city: str = input("Enter city: ")
 country: str = input("Enter country: ")
 asyncio.run(NominatimAPI.search_address(formate="json", city=city, street=street, country=country))
+
